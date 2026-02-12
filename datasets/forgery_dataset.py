9550abdc653dad78b674843c38425fc1acdadc3e
WijdanHaider <120770010+WijdanHaider@users.noreply.github.com>
2026-02-12 13:10:01 +0500

    Create forgery_dataset.py

---
+++ datasets/forgery_dataset.py
@@ -0,0 +1,29 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import logging
+
+class ForgeryDataset(Dataset):
+    def __init__(self, image_paths, labels, spatial_transform, freq_transform):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.spatial_transform = spatial_transform
+        self.freq_transform = freq_transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        try:
+            image = Image.open(img_path).convert("RGB")
+        except Exception:
+            logging.warning(f"Could not load {img_path}")
+            dummy = torch.randn(3,224,224)
+            return dummy, dummy, torch.tensor(-1.0)
+
+        spatial_img = self.spatial_transform(image)
+        freq_img = self.freq_transform(image)
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
+
+        return spatial_img, freq_img, label
